4cb4dc6cfbfd5ba7e74febbf5eba98177c20ac52
Hyann <contato.hyann@gmail.com>
2025-05-20 11:46:09 -0300

    Adicionando javascript

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //Declarando as variaveis usando DOM
+const loginInorm = document.getElementById('form-login')
+const mensagem = document.getElementById('message')
+const video = document.querySelector('.video-container')
+const loginContainer = document.getElementById('login-container')
+const botaoEntrar = document.getElementById('btnEntrar')
+const botaoSair = document.getElementById('btnSair')
+
+//Verifica se o usuario está n localstorage
+
+})
